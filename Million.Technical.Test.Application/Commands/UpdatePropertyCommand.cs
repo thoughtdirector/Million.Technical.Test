a181f618af0eee792747cff9f626e87be986cf23
Angel <>
2025-01-27 16:46:45 -0500

    chore(architecture): run code cleanup for consistent formatting

--- Million.Technical.Test.Application/Commands/UpdatePropertyCommand.cs
+++ Million.Technical.Test.Application/Commands/UpdatePropertyCommand.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Million.Technical.Test.Application.Commands.Models;
+﻿using Million.Technical.Test.Application.Commands.Models;
 using Million.Technical.Test.Libraries.Cqs.Request.Commands;
 
 namespace Million.Technical.Test.Application.Commands
