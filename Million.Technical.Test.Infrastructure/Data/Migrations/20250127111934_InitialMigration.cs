ada33a870316782c6cb6fad5048ae9016e8fa65d
Angel <>
2025-01-27 06:25:50 -0500

    feat(database): add initial database schema and migrations

---
+++ Million.Technical.Test.Infrastructure/Data/Migrations/20250127111934_InitialMigration.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Million.Technical.Test.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Owners",
+                columns: table => new
+                {
+                    IdOwner = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Photo = table.Column<byte[]>(type: "varbinary(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Owners", x => x.IdOwner);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Properties",
+                columns: table => new
+                {
+                    IdProperty = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    CodeInternal = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    IdOwner = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Properties", x => x.IdProperty);
+                    table.ForeignKey(
+                        name: "FK_Properties_Owners_IdOwner",
+                        column: x => x.IdOwner,
+                        principalTable: "Owners",
+                        principalColumn: "IdOwner",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyImages",
+                columns: table => new
+                {
+                    IdPropertyImage = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ImageData = table.Column<byte[]>(type: "varbinary(max)", nullable: true),
+                    Enabled = table.Column<bool>(type: "bit", nullable: false),
+                    IsPrimary = table.Column<bool>(type: "bit", nullable: false),
+                    IdProperty = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyImages", x => x.IdPropertyImage);
+                    table.ForeignKey(
+                        name: "FK_PropertyImages_Properties_IdProperty",
+                        column: x => x.IdProperty,
+                        principalTable: "Properties",
+                        principalColumn: "IdProperty",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PropertyTraces",
+                columns: table => new
+                {
+                    IdPropertyTrace = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateSale = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Value = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Tax = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IdProperty = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PropertyTraces", x => x.IdPropertyTrace);
+                    table.ForeignKey(
+                        name: "FK_PropertyTraces_Properties_IdProperty",
+                        column: x => x.IdProperty,
+                        principalTable: "Properties",
+                        principalColumn: "IdProperty",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Properties_IdOwner",
+                table: "Properties",
+                column: "IdOwner");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PropertyImages_IdProperty",
+                table: "PropertyImages",
+                column: "IdProperty");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PropertyTraces_IdProperty",
+                table: "PropertyTraces",
+                column: "IdProperty");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PropertyImages");
+
+            migrationBuilder.DropTable(
+                name: "PropertyTraces");
+
+            migrationBuilder.DropTable(
+                name: "Properties");
+
+            migrationBuilder.DropTable(
+                name: "Owners");
+        }
+    }
+}
--- Million.Technical.Test/DependencyInjection/BaseInjections.cs
+++ Million.Technical.Test/DependencyInjection/BaseInjections.cs
@@ -11,7 +11,7 @@ namespace Million.Technical.Test.Libraries.DependencyInjection
         public static void InjectBase(this WebApplicationBuilder builder)
         {
             builder.Services.AddDbContext<DbContext, RealEstateDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("WebApiDatabase"), b => b.MigrationsAssembly("Million.Technical.Test.Infrastructure")), ServiceLifetime.Transient);
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("Million.Technical.Test.Infrastructure")), ServiceLifetime.Transient);
 
             builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             builder.Services.AddScoped<IImageService>(provider => new ImageService());
